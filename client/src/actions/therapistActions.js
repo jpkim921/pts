fc72312ec041b4c3a9aab57a6ea696f964dda118
Jayon Kim <jpkim921@gmail.com>
2019-05-30 23:41:27 -0400

    add createTherapist action

--- client/src/actions/therapistActions.js
+++ client/src/actions/therapistActions.js
@@ -1,8 +1,28 @@
+
+
+
 export function fetchTherapists() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_THERAPISTS' });
     return fetch('/therapists')
       .then(response => response.json())
-      .then(responseJson => dispatch({ type: 'FETCH_THERAPISTS', payload: responseJson }));      
+      .then(responseJson => dispatch({ type: 'FETCH_THERAPISTS', payload: responseJson }));
+  };
+}
+
+export function createTherapist(data) {
+  return (dispatch) => {
+    dispatch({ type: 'CREATING_THERAPIST' });
+    return fetch('/therapists', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+    // .then(therapist => dispatch(addTherapist(therapist)))
+    .then(therapist => console.log('Success:', therapist))
+    .catch(error => console.error('Error:', error));
+      // .then(responseJson => dispatch({ type: 'FETCH_THERAPISTS', payload: responseJson }));
   };
 }
