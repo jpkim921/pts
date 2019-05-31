259386dccdb0ceab8bbbc02c7c9bb971230a4eb7
Jayon Kim <jpkim921@gmail.com>
2019-05-30 23:04:40 -0400

    creating greetings page

--- client/src/components/Greetings.js
+++ client/src/components/Greetings.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+//
+// export const Greetings = () => {
+//
+//   return (
+//     <div>
+//       <h2>Sign Up || Login</h2>
+//       <nav>
+//         <Link to="/signup">Sign Up</Link>
+//         <Link to="/login">Login</Link>
+//       </nav>
+//     </div>
+//   );
+// };
 
-export const Greetings = () => {
-  return (
-    <div>
-      <h2>Sign Up || Login</h2>
-      <nav>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Login</Link>
-      </nav>
-    </div>
-  );
-};
+
+class Greetings extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>Sign Up || Login</h2>
+        <nav>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+      </div>
+    )
+  }
+}
+
+
+export default Greetings
